de08de18b677378ed3c681d97b8fd33a19ec617b
Mammadov Elmar <mamedovelmar93@gmail.com>
2021-10-28 14:16:13 +0200

    Tamamlandi

--- FrontEndProject/Js/myproject.js
+++ FrontEndProject/Js/myproject.js
@@ -15,3 +15,21 @@ function rightCover() {
     textCover1.style.display = 'none'
     textCover2.style.display = 'block'
 }
+
+let x = 0
+let move = document.querySelector('.sliders')
+
+function left() {
+    x = x + 1200
+    move.style.left = `${x}px`
+
+
+
+}
+
+function right() {
+    x = x - 1200
+    move.style.left = `${x}px`
+    s
+
+}
