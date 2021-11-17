65325e601b2d1491a3f2119a21f2ab1afa7d840f
Mammadov Elmar <mamedovelmar93@gmail.com>
2021-11-17 13:39:25 +0100

    Practise With OS Module / File Handling

---
+++ Work/Python/OsModule/osmodtest.py
@@ -0,0 +1,73 @@
+# import os
+# x=os.getcwd
+# print(x)                                          # it is to check the path
+
+
+
+
+
+
+
+# import os
+# newfolder="C:/Users/elmar/Desktop/almapapaksi"
+# os.mkdir(newfolder)                               # it is  to create a folder
+
+
+
+
+
+
+# import os
+# deletethefolder="C:/Users/elmar/Desktop/aaaaaaa"
+# os.rmdir(deletethefolder)                        # it is to delete the folder, but it is already been deleted
+
+
+
+
+
+# import os
+
+# qoyulduguUnvan="C:/sssnewP/yenipap" 
+
+# os.makedirs(qoyulduguUnvan) #                   it will create the missing folders
+
+
+
+
+
+
+
+
+
+                                               # FILE HANDLING
+# import os           
+# yol = "C:/Users/elmar/Desktop/almapapaksi"
+# x=os.path.join(yol, "filedana.txt")            # Ya doljen zakomnetiurovat  ctob sleduwiy kod zarabotal
+# file=open(x, "w")
+# file.write('Men programciyamda indi oz alemimde')
+# file.close()
+
+# os.remove(x)                           #POCEMU YA NE MOGU UDALIT FILE.... NO ctob eqo udalit mi doljni imenno ukazat eqo path???
+# os.rename(yol, "newname")            # pocemu ya koqda vvoju etot kod on SOZDAET pereimennovaniy file v OSMOdule, a ne v paple kotoroyt ya xocu
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import os
+# yol = "C:/Users/elmar/Desktop/almapapaksi"
+# y=os.path.join(yol, "ada.html")
+# file=open(y, "w")
+# file.write('sss')            # BU melumat hara duwuwrki?  i posleduyuwiie 2 tak je sdelat
+# file.close()
+# os.remove(y)
+
+
